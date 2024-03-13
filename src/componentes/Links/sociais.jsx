0c79e905297de8d784a5568154530a6c5bc0c1aa
Leidejane Santos <elzosantos006@gmail.com>
2024-03-13 11:46:40 -0300

    Atualizando3

--- src/componentes/Links/sociais.jsx
+++ src/componentes/Links/sociais.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from "react-icons/fa";
-
+import { BsFillPersonLinesFill } from "react-icons/bs";
 const Sociais = () => {
 
   const links = [
