2dcafc8f0aac5d7027adda8fed25acf6eb9eca92
Elzo1 <elzosantos006@gmail.com>
2024-12-30 23:48:20 -0300

    Ultima-Atualização

--- src/componentes/Projetos/projetos.jsx
+++ src/componentes/Projetos/projetos.jsx
@@ -5,12 +5,16 @@ import {
   SiMysql,
   SiSpringboot,
   SiJsonwebtokens,
+  SiJunit5,
+  SiApachekafka,
+  SiMongodb
 } from 'react-icons/si';
+
+import { FaExchangeAlt } from 'react-icons/fa';
 import { GrDocker } from 'react-icons/gr';
 import { FaGitSquare } from 'react-icons/fa';
 
 const Projetos = () => {
-  // Array de projetos, cada projeto possui suas propriedades como título, descrição, tecnologias e link.
   const projetos = [
     {
       id: 1,
@@ -26,14 +30,15 @@ const Projetos = () => {
           <SiJsonwebtokens size={50} className="text-gray-950 px-1 hover:scale-105 duration-200" />
           <GrDocker size={50} className="text-blue-700 px-1 hover:scale-105 duration-200" />
           <FaGitSquare size={50} className="hover:text-red-600 hover:scale-105 duration-200" />
+          
         </>
       ),
-      link: 'https://github.com/Elzo1/Loja-Virtual-Back-End', // Link para o repositório do projeto
+      link: 'https://github.com/Elzo1/Loja-Virtual-Back-End',
     },
     {
       id: 2,
       titu2: 'PDV',
-      par: 'Projeto de back-end para PDV de vendas com Spring Security e JWT. Suporta múltiplos usuários com associação por ID. Para que cada um tenha acesso aos seus dados',
+      par: 'Projeto de back-end para PDV de vendas com Spring Security e JWT. Suporta múltiplos usuários com associação por ID.',
       titu: 'Tecnologias usadas:',
       imgs: (
         <>
@@ -45,7 +50,7 @@ const Projetos = () => {
           <FaGitSquare size={50} className="hover:text-red-600 hover:scale-105 duration-200" />
         </>
       ),
-      link: 'https://github.com/Elzo1/PDV-de-vendas-de-produtos-/tree/main/src', // Link para o repositório do projeto
+      link: 'https://github.com/Elzo1/PDV-de-vendas-de-produtos-/tree/main/src',
     },
     {
       id: 3,
@@ -61,35 +66,44 @@ const Projetos = () => {
           <FaGitSquare size={50} className="hover:text-red-600 hover:scale-105 duration-200" />
         </>
       ),
-      link: 'https://github.com/Elzo1/Sistema-de-Cadastro-de-Pessoas', // Link para o repositório do projeto
+      link: 'https://github.com/Elzo1/Sistema-de-Cadastro-de-Pessoas',
+    },
+    {
+      id: 4,
+      titu2: 'Sistema de rastreamento de veículos',
+      par: 'Sistema de rastreamento de veículos em tempo real com Spring Boot, MongoDB, Kafka, WebSockets e Junit5 no back-end, e uma interface intuitiva com ReactJS + Vite',
+      titu: 'Tecnologias usadas:',
+      imgs: (
+        <>
+          <SiSpringboot size={50} className="text-green-700 px-1 hover:scale-105 duration-200" />
+          <SiJunit5 size={50} className="text-blue-700 px-1 hover:scale-105 duration-200" />
+          <FaGitSquare size={50} className="hover:text-red-600 hover:scale-105 duration-200" />
+          <SiApachekafka size={50} className="hover:text-blue-700 hover:scale-105 duration-200" />
+          <FaExchangeAlt size={50} className="text-blue-500 hover:scale-105 duration-200" />
+          <SiMongodb size={50} className="text-green-700 px-1 hover:scale-105 duration-200" />
+        </>
+      ),
+      link: 'https://github.com/Elzo1/Sistema-de-rastreamento-de-veiculo-fullstack',
     },
   ];
 
   return (
     <div id="projetos" className="bg-gradient-to-b from-black to-gray-800 w-full text-white min-h-screen">
-      {/* Seção de Projetos */}
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        {/* Título da Seção */}
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">Projetos</p>
           <p className="py-6">Confira alguns projetos aqui</p>
         </div>
-        {/* Grid de Cards */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 px-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 px-4 place-items-stretch">
           {projetos.map(({ id, imgs, titu, par, titu2, link }) => (
             <div
               key={id}
-              className="hover:border-y-2 shadow-2xl hover:shadow-gray-600 hover:scale-105 duration-500 rounded-2xl p-4 bg-gray-900 w-full sm:w-[90%] max-w-sm mx-auto"
+              className="hover:border-y-2 shadow-2xl hover:shadow-gray-600 hover:scale-105 duration-500 rounded-2xl p-4 bg-gray-900 flex flex-col h-full"
             >
-              {/* Título do Projeto */}
               <h2 className="mt-2 font-semibold text-3xl">{titu2}</h2>
-              {/* Descrição do Projeto */}
-              <p className="mt-5">{par}</p>
-              {/* Tecnologias Usadas */}
+              <p className="mt-5 flex-grow">{par}</p>
               <p className="mt-3">{titu}</p>
-              {/* Ícones das Tecnologias */}
               <div className="flex mt-2">{imgs}</div>
-              {/* Botão para o Código */}
               <a href={link} target="_blank" rel="noopener noreferrer">
                 <button className="text-white w-fit px-4 py-2 my-3 rounded-md bg-gradient-to-r from-cyan-400 to-blue-600 font-semibold hover:scale-110 duration-300 shadow-md">
                   Ir para o Código
@@ -104,3 +118,4 @@ const Projetos = () => {
 };
 
 export default Projetos;
+  
--- src/componentes/Sobre/sobre.jsx
+++ src/componentes/Sobre/sobre.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export const Sobre = () => {
   return (
-    <div id="sobre" className='w-full h-screen bg-gradient-to-b  to-black from-gray-800 text-white '>
-      <div className='max-w-screen-lg p- mx-auto flex flex-col justify-center w-full h-full'>
-        <div className='pb-8 max-sm:mt-72  md:mt-1 '>
-
-          <p className='text-4xl font-bold inline border-b-4 border-gray-500 max-sm:m-36 '>Sobre</p>
-
+    <div
+      id="sobre"
+      className="w-full h-screen bg-gradient-to-b  to-black from-gray-800 text-white "
+    >
+      <div className="max-w-screen-lg p- mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8 max-sm:mt-72  md:mt-1 ">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500 max-sm:m-36 ">
+            Sobre
+          </p>
         </div>
 
-        <p  className='mr-2 ml-2'>
-          Oi, me chamo Elzo e sou desenvolvedor Back-End Jr, com conhecimento intermediário em Front-End.
-          Meu objetivo é continuar crescendo como desenvolvedor, explorando novas tecnologias e contribuindo para projetos desafiadores que fazem a diferença.
+        <p className="mr-2 ml-2">
+          Olá! Sou Elzo, desenvolvedor Back-End Jr. apaixonado por inovação. Com
+          mais de 1 ano de experiência em HTML, CSS, Tailwind, React, Java,
+          Spring Boot, Hibernate, JPA, MySQL, MongoDB, Docker e Linux, estou
+          ansioso para: Aprofundar conhecimentos em arquitetura de sistemas
+          Desenvolver soluções escaláveis e seguras Contribuir para projetos
+          open-source Aprender novas tecnologias Colaborar em equipes ágeis e
+          inovadoras
         </p>
         <br />
-
-        <p className='mr-2 ml-2'>
-        Com um pouco mais de 1 ano de experiência com Html, Css, Tailwind, React, Java, Spring boot, Hibernate, Jpa, Mysql, Mongodb, Docker, Linux.
-        </p>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Sobre
+export default Sobre;
